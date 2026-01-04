feceb807eca06df78ca97b1f1c21262f046a3208
Dear.Va <dear.va.love@gmail.com>
2026-01-05 00:39:51 +0800

    perf: cahce FieldInfo for better performance

--- src/WritableJsonConfiguration/WritableJsonConfigurationProviderExtensions.cs
+++ src/WritableJsonConfiguration/WritableJsonConfigurationProviderExtensions.cs
@@ -6,55 +6,62 @@ namespace Microsoft.Extensions.Configuration;
 
 public static class WritableJsonConfigurationProviderExtensions
 {
-    /// <summary>
-    /// Set value for current section
-    /// </summary>
+    private static readonly FieldInfo ConfigurationSectionRootFieldInfo =
+        typeof(ConfigurationSection).GetField("_root", BindingFlags.NonPublic | BindingFlags.Instance) ??
+        throw new InvalidOperationException("Could not find _root field in ConfigurationSection");
+
     /// <param name="configuration"></param>
-    /// <param name="value"></param>
-    /// <exception cref="ArgumentOutOfRangeException"></exception>
-    public static void Set(this IConfiguration configuration, object? value)
+    extension(IConfiguration configuration)
     {
-        switch (configuration)
+        /// <summary>
+        /// Set value for current section
+        /// </summary>
+        /// <param name="value"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void Set(object? value)
         {
-            case IConfigurationRoot configurationRoot:
+            switch (configuration)
             {
-                var provider = configurationRoot.Providers.OfType<WritableJsonConfigurationProvider>().First();
-                provider.Set(null, value);
-                break;
+                case IConfigurationRoot configurationRoot:
+                {
+                    var provider = configurationRoot.Providers.OfType<WritableJsonConfigurationProvider>().First();
+                    provider.Set(null, value);
+                    break;
+                }
+                case ConfigurationSection configurationSection:
+                {
+                    var root = (IConfigurationRoot)ConfigurationSectionRootFieldInfo.GetValue(configurationSection)!;
+                    var provider = root.Providers.OfType<WritableJsonConfigurationProvider>().First();
+                    provider.Set(configurationSection.Path, value);
+                    break;
+                }
+                default:
+                {
+                    throw new NotSupportedException($"Configuration of type {configuration.GetType().FullName} is not supported");
+                }
             }
-            case ConfigurationSection configurationSection:
-            {
-                var rootProp = typeof(ConfigurationSection).GetField("_root", BindingFlags.NonPublic | BindingFlags.Instance);
-                var root = (IConfigurationRoot)rootProp!.GetValue(configurationSection)!;
-                var provider = root.Providers.OfType<WritableJsonConfigurationProvider>().First();
-                provider.Set(configurationSection.Path, value);
-                break;
-            }
-            default:
-                throw new ArgumentOutOfRangeException(nameof(configuration));
+        }
+
+        /// <summary>
+        /// Get section and set value
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="value"></param>
+        public void Set(string section, object? value)
+        {
+            configuration.GetSection(section).Set(value);
+        }
+
+        /// <summary>
+        /// Get object by section
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="section"></param>
+        /// <returns></returns>
+        public T? Get<T>(string section)
+        {
+            return configuration.GetSection(section).Get<T>();
         }
     }
 
-    /// <summary>
-    /// Get section and set value
-    /// </summary>
-    /// <param name="configuration"></param>
-    /// <param name="section"></param>
-    /// <param name="value"></param>
-    public static void Set(this IConfiguration configuration, string section, object? value)
-    {
-        configuration.GetSection(section).Set(value);
-    }
-
-    /// <summary>
-    /// Get object by section
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="configuration"></param>
-    /// <param name="section"></param>
-    /// <returns></returns>
-    public static T? Get<T>(this IConfiguration configuration, string section)
-    {
-        return configuration.GetSection(section).Get<T>();
-    }
 }
